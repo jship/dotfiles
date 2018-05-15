1534ea54e5c5659fc6891ec40867f0453fbeb8c1
Jason Shipman <jasonpshipman@gmail.com>
2018-05-15 16:31:36 -0400

    Add ghcid -> neovim auto-navigation

---
+++ .local/bin/ghcid-go-to-error.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# If you have vim in pane 0 and ghcid in pane 1, this will jump to the first error/warning
+
+OP=`tmux capture-pane -p -t 1 | vims -s 'v/:\<enter>nnyVGp:%s/\\\\n//g\<enter>0f:d$0Ps) \<esc>0s+call\\\\ cursor(\<esc>f:r,'`
+
+tmux send-keys Escape
+tmux send-keys ":e $OP"
+tmux send-keys Enter
+tmux send-keys 'zz'
