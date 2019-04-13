bf0f82cbdb1dabec06f5e57a7517542df550e16a
Jason Shipman <jasonpshipman@gmail.com>
2019-04-13 15:09:50 -0400

    Update ghcid-go-to-error.hs for ghcid v0.7.x msg format

--- .local/bin/ghcid-go-to-error.sh
+++ .local/bin/ghcid-go-to-error.sh
@@ -2,7 +2,11 @@
 
 # If you have vim in pane 0 and ghcid in pane 1, this will jump to the first error/warning
 
-OP=`tmux capture-pane -p -t 1 | vims -s 'v/:\<enter>nnyVGp:%s/\\\\n//g\<enter>0f:d$0Ps) \<esc>0s+call\\\\ cursor(\<esc>f:r,'`
+# v0.6.x message format
+#OP=`tmux capture-pane -p -t 1 | vims -s 'v/:\<enter>nnyVGp:%s/\\\\n//g\<enter>0f:d$0Ps) \<esc>0s+call\\\\ cursor(\<esc>f:r,'`
+
+# v0.7.x message format
+OP=`tmux capture-pane -p -t 1 | vims -s 'v/:\<enter>n/:\\\\|-\<enter>yVGp:%s/\\\\n//g\<enter>0f:d$0Ps) \<esc>0s+call\\\\ cursor(\<esc>f:r,'`
 
 tmux send-keys Escape
 tmux send-keys ":e $OP"
